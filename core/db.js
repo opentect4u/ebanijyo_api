59d7aa46f3d226e31a3aafc74f34be768def6424
Subham0529 <samantasubham9804@gmail.com>
2022-05-18 12:29:40 +0530

    API for ebanijyo subham 18-05-2022

--- core/db.js
+++ core/db.js
@@ -9,13 +9,22 @@ const mysql = require('mysql');
 //     database: 'ebanijya'
 // });
 
-// For Server
+// For Server https://apiebanijya.opentech4u.co.in
+// const db = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: 'ebanijya_user',
+//     password: 'SynergicWeb@2020',
+//     database: 'ebanijya'
+// });
+
+// For Server https://api.ebanijyo.com
 const db = mysql.createPool({
     connectionLimit: 10,
     host: 'localhost',
-    user: 'ebanijya_user',
-    password: 'SynergicWeb@2020',
-    database: 'ebanijya'
+    user: 'ebanijyo_user',
+    password: 'Ebanijyo@2022',
+    database: 'ebanijyo'
 });
 
 db.getConnection((err, connection) => {
